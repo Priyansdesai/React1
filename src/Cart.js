0359417da7d0be4fc0928cabfdf15985e9f17f5f
Priyans Desai <priyansdesai@berkeley.edu>
2019-03-22 12:46:30 -0700

    Final Part 1

--- src/Cart.js
+++ src/Cart.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./styles/cart.css";
+import Product from "./Product.js";
+import ProductData from "./Data";
 
 class Cart extends React.Component {
 
   render() {
     return (
         <div className="page-content">
+        	{this.renderProducts()}
             <h2>Add your products here!</h2>
         </div>
     );
   }
 
-}
+  renderProducts() {
+  	return ProductData.products.map(product => {
+  		return <Product price={product.cost} name={product.name} limit={product.stock} />;
+  	});
+  }
 
+}
 export default Cart;
---
+++ src/Product.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles/cart.css";
+let numberClicks = 0
+
+
+const Product = props =>{
+	return (
+		<div className="ui cards">
+			<div className="card">
+    			<div className="content">
+	      			<div className="header">{props.name}</div>
+	      			<div className="description"> {props.price} </div>
+	    			<div className="ui bottom attached button" onClick={() => addToCart(props)}>
+	      				<i className="add icon"></i> Add to Cart 
+	  				</div>
+	  			</div>
+  			</div>
+  		</div>
+  		);
+}
+function addToCart (props) {
+  if (numberClicks >= props.limit  ) {
+    alert("There are " + numberClicks + " apples in your cart.");
+  }
+  else if (props.limit === 0) {
+  	alert('Out of stock!')
+  }
+  else {
+  	numberClicks++;
+  	alert(props.name + " added!")
+  }
+}
+
+export default Product;
